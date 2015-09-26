0582d2d487bb42e7e48eb7d08f864179d6b1ddb5
Robin Linus <robin_woll@capira.de>
2015-09-26 18:59:35 -0400

    Build after merge....

--- dist/slang.js
+++ dist/slang.js
@@ -1,30 +1,5 @@
 'use strict';
 var Slang = Slang || {};
-Slang.colorslang = (function() {
-    function compare(expected, given) {
-        return expected === rgbToHex(given);
-    }
-
-    function rgbToHex(c) {
-        return '#' + ((1 << 24) + (c[0] << 16) + (c[1] << 8) + c[2]).toString(16).slice(1);
-    }
-    /*
-        function hexToRgb(hex) {
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-            } : null;
-        }
-    */
-    return {
-        compare: compare
-    };
-})();
-
-'use strict';
-var Slang = Slang || {};
 Slang.hausdorffslang = (function() {
 
     function compare(X, Y, translateX, translateY, scale) {
@@ -135,6 +110,31 @@ Slang.hausdorffslang = (function() {
         };
     }
 
+    return {
+        compare: compare
+    };
+})();
+
+'use strict';
+var Slang = Slang || {};
+Slang.colorslang = (function() {
+    function compare(expected, given) {
+        return expected.toLowerCase() === rgbToHex(given).toLowerCase();
+    }
+
+    function rgbToHex(c) {
+        return '#' + ((1 << 24) + (c[0] << 16) + (c[1] << 8) + c[2]).toString(16).slice(1);
+    }
+    /*
+        function hexToRgb(hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        }
+    */
     return {
         compare: compare
     };
