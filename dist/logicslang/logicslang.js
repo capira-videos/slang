ddc021ead77ac3a2f149fb5f0e63403805aab1f9
Robin Linus <robin_woll@capira.de>
2015-10-27 16:37:56 +0100

    Build mathslang...

--- dist/logicslang/logicslang.js
+++ dist/logicslang/logicslang.js
@@ -3,15 +3,20 @@ window.Slang = window.Slang || {};
 window.Slang.logicslang = {
     // Handles logical operators #or, #and, and #not
     compare: function(expectedValue, givenValue, compareFn, _units) {
-    	// Replace any @variableName with the variable in givenValue
-    	if (typeof givenValue == "object") {
-    		for(var key in givenValue) {
-	    		var regexp = new RegExp("@" + key, "g");
-	    		expectedValue = expectedValue.replace(regexp, givenValue[key]);
-    		}
-    	}
-        var lp = new LogicParser(compareFn);
-        return lp.compare(expectedValue, givenValue, _units);
+        // Replace any @variableName with the variable in givenValue
+        if (typeof givenValue == 'object') {
+            for (var key in givenValue) {
+                var regexp = new RegExp('@' + key, 'g');
+                if (expectedValue.exp) {
+                    expectedValue.inputExp = expectedValue.inputExp.replace(regexp, givenValue[key]);
+                } else {
+                    expectedValue = expectedValue.replace(regexp, givenValue[key]);
+                }
+            }
+            return compareFn(expectedValue, expectedValue.inputExp, _units);
+        }
+        //var lp = new LogicParser(compareFn);
+        return compareFn(expectedValue, givenValue, _units);
     },
 };
 
